0a6c1a236d757c4a85d91af09c302bae9323e0a0
Evan Bremer <evanbb@gmail.com>
2021-01-01 22:21:09 -0600

    add react index, initial publish

--- rollup.config.js
+++ rollup.config.js
@@ -1,1 +1,10 @@
-export default {}
+import fs from 'fs'
+import typescript from '@rollup/plugin-typescript'
+
+export default fs.readdirSync('packages').map(dir => ({
+  input: `packages/${dir}/src/index.ts`,
+  output: {
+    dir: `packages/${dir}/lib`
+  },
+  plugins: [typescript()]
+}))
