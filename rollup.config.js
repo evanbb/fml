4de6d479b61afe98eb0dcaeaa2cf00eb7e487dc9
Evan Bremer <evanbb@gmail.com>
2021-03-22 21:19:23 -0500

    add react start script

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,15 @@
 import fs from 'fs'
 import typescript from '@rollup/plugin-typescript'
 
+const tsPluginConfig = process.env.ROLLUP_WATCH ? {noEmitOnError: false} : {}
+
 export default fs.readdirSync('packages').map(dir => ({
   input: `packages/${dir}/src/index.ts`,
   output: {
     dir: `packages/${dir}/lib`
   },
   plugins: [
-    typescript(),
+    typescript(tsPluginConfig),
     {
       name: 'rollup-plugin-copy-types',
       async writeBundle() {
