f907ca201f3e90bc5ad16d99cddd5721a52ee28c
Evan Bremer <evanbb@gmail.com>
2021-09-23 08:36:08 -0500

    fix field stories

--- packages/react/src/Field.stories.tsx
+++ packages/react/src/Field.stories.tsx
@@ -1,77 +1,89 @@
-import '@fml/add/validators/required'
+import '@fml/add/validators/required';
 import Field from './Field';
 
 export const CheckboxExample = () => (
   <Field<boolean>
-    config={{
-      label: 'A checkbox',
-      control: 'checkbox',
-      validators: [['required', 'Check this, please']],
-      defaultValue: false,
-    }}
+    config={[
+      'fml:checkbox',
+      {
+        label: 'A checkbox',
+        validators: [['required', 'Check this, please']],
+        defaultValue: false,
+      },
+    ]}
   />
 );
 
 export const DateExample = () => (
   <Field<Date | undefined>
-    config={{
-      label: 'A date',
-      control: 'date',
-      defaultValue: undefined,
-    }}
+    config={[
+      'fml:date',
+      {
+        label: 'A date',
+        defaultValue: undefined,
+      },
+    ]}
   />
 );
 export const DateTimeExample = () => (
   <Field<Date>
-    config={{
-      label: 'A date',
-      control: 'datetime',
-      defaultValue: new Date(),
-    }}
+    config={[
+      'fml:datetime',
+      {
+        label: 'A date',
+        defaultValue: new Date(),
+      },
+    ]}
   />
 );
 export const HiddenExample = () => (
   <Field<string>
-    config={{
-      label: 'A hidden value',
-      control: 'hidden',
-      defaultValue: '',
-    }}
+    config={[
+      'fml:hidden',
+      {
+        label: 'A hidden value',
+        defaultValue: '',
+      },
+    ]}
   />
 );
 export const NumberExample = () => (
   <Field<number>
-    config={{
-      label: 'A number',
-      control: 'number',
-      validators: [['required', 'This is required, dawg']],
-      defaultValue: undefined,
-    }}
+    config={[
+      'fml:number',
+      {
+        label: 'A number',
+        validators: [['required', 'This is required, dawg']],
+        defaultValue: undefined,
+      },
+    ]}
   />
 );
 export const SelectExample = () => (
   <Field<'a' | 'b' | 'c'>
-    config={{
-      label: 'A few options',
-      control: 'select',
-      options: { a: 'Option A', b: 'Option B', c: 'Option C' },
-      defaultValue: 'b',
-    }}
+    config={[
+      'fml:select',
+      {
+        label: 'A few options',
+        options: { a: 'Option A', b: 'Option B', c: 'Option C' },
+        defaultValue: 'b',
+      },
+    ]}
   />
 );
 export const TextInputExample = () => (
   <Field<string>
-    config={{ label: 'Text input', control: 'text', defaultValue: '' }}
+    config={['fml:text', { label: 'Text input', defaultValue: '' }]}
   />
 );
 export const TextAreaInputExample = () => (
   <Field<string>
-    config={{ label: 'Text area', control: 'textarea', defaultValue: '' }}
+    config={['fml:textarea', { label: 'Text area', defaultValue: '' }]}
   />
 );
 export const ToggleExample = () => (
   <Field<boolean>
-    config={{ label: 'A checkbox', control: 'toggle', defaultValue: false }}
+    config={['fml:toggle', { label: 'A checkbox', defaultValue: false }]}
   />
 );
 
