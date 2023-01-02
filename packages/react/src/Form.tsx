ea893b517232c0b4543fb2c94237ff65f8ad3c8d
Evan Bremer <evanbb@gmail.com>
2023-01-02 13:47:06 -0600

    resolves #11

--- packages/react/src/Form.tsx
+++ packages/react/src/Form.tsx
@@ -4,14 +4,14 @@ import { FormEvent, useState } from 'react';
 import FmlComponent, { getControlConfig } from './common/FmlComponent';
 
 interface submitCallback<TModel> {
-  (model: TModel, event: FormEvent<HTMLFormElement>): void;
+  (event: FormEvent<HTMLFormElement>, model: TModel): void;
 }
 
 interface FormProps<TModel> {
   config: FmlConfiguration<TModel>;
   formName: string;
-  onSubmit: submitCallback<TModel>;
-  submitText: string;
+  onSubmit?: submitCallback<TModel>;
+  submitText?: string;
 }
 
 export default function Form<TModel>({
@@ -20,18 +20,28 @@ export default function Form<TModel>({
   onSubmit,
   submitText,
 }: FormProps<TModel>) {
-  const [value, seValue] = useState<FmlValueState<TModel | undefined>>({
+  const [value, setValue] = useState<FmlValueState<TModel | undefined>>({
     value: getControlConfig(config).defaulValue as TModel | undefined,
     validity: 'unknown',
   });
   const { validity, value: innerValue } = value;
 
-  return (
-    <form onSubmit={(e) => onSubmit(innerValue as TModel, e)}>
-      <FmlContextProvider<TModel> localControlId={formName} onChange={seValue}>
-        <FmlComponent config={config} />
-      </FmlContextProvider>
-      <input value={submitText} type='submit' disabled={validity !== 'valid'} />
+  const content = (
+    <FmlContextProvider<TModel> localControlId={formName} onChange={setValue}>
+      <FmlComponent config={config} />
+    </FmlContextProvider>
+  );
+
+  return onSubmit ? (
+    <form onSubmit={(e) => onSubmit?.(e, innerValue as TModel)}>
+      {content}
+      <input
+        value={submitText || 'Submit'}
+        type='submit'
+        disabled={validity !== 'valid'}
+      />
     </form>
+  ) : (
+    content
   );
 }
